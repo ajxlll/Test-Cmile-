9592966c68d81b1c86a044a93733f135f59708c6
Marco <>
2022-07-05 20:14:45 -0700

    add telegram

--- site/pages/contact.js
+++ site/pages/contact.js
@@ -6,7 +6,9 @@ export default function Contact() {
         <Layout>
             <h1 className="text-4xl"> Contact</h1>
             <Link href="mailto:CryptoMiler@gmail.com"> cryptomiler@gmail </Link>
-"
+            <p>
+                <Link href="https://t.me/cryptomiler"> Telegram </Link>
+            </p>
         </Layout>
     )
 }
