c6b8db20ac3d865f46b83ad94a3af2f3081a9284
Marco <>
2022-07-23 16:16:16 -0700

    add border, move button, and add placeholder text on NFT section

--- site/pages/index.js
+++ site/pages/index.js
@@ -288,18 +288,27 @@ export default function Home() {
                 </VerticalTimeline>
               </section>
 
+              <a id="nft"></a>
               <section>
-                <a id="nft"></a>
-                <div className="mt-20 flex flex-wrap items-center">
-                  <div className="w-full sm:w-1/2 text-center px-6">
-                    <h1 className="mt-44 text-4xl text-black"> NFT section</h1>
+                <div className="mt-40 border-2 w-2/4 mx-auto rounded-md flex flex-wrap">
+                  <div className="mx-auto text-center">
+                    <h1 className="text-center mt-12 mb-6 text-4xl text-black"> NFT Marketplace</h1>
+                    <p className="text-black lg:px-60 text-center font-light">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse bibendum, nunc non posuere consectetur,
+                      justo erat semper enim, non hendrerit dui odio id enim.
+                    </p>
+                    <p className="text-black lg:px-60 mt-12 text-center font-light">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse bibendum, nunc non posuere consectetur,
+                      justo erat semper enim, non hendrerit dui odio id enim.
+                    </p>
                     <Link href="https://gateway.ipfscdn.io/ipfs/QmZ7JB3mBYxTD8McJZK8QrVAY7i9JrL3Tqu14GVaYYqnQh/marketplace.html?contract=0x622841B23F77b17E61d59f1F5cD7c69809EC89f7&chainId=80001&listingId=0">
-                      <button className="mb-12 transition p-2 ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-lg">
+                      <button className="mb-12 transition text-white p-2 m-2 mt-8 ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-lg">
                         MARKETPLACE
                       </button>
                     </Link>
                   </div>
-                  <div className="w-full sm:w-1/2 p-6 mb-8"></div>
                 </div>
               </section>
 
